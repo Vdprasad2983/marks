952949b15298a83604937a86c348b4f173eaac10
Vdprasad2983 <99181740+Vdprasad2983@users.noreply.github.com>
2024-09-27 22:20:29 +0530

    Update feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -64,21 +64,4 @@ if submit_button:
                 (yourname, father_name, roll_number, college, department, section, cgpa, percentage, feedback, date))
     conn.commit()
     st.success("Data added into the database successfully !!!")
-
-# Button to view feedback data
-'''view_data_button = st.button("View Feedback Data")
-
-if view_data_button:
-    # Fetch all data from the feedback table
-    cur.execute("SELECT * FROM feedback")
-    rows = cur.fetchall()
-
-    if rows:
-        st.write("Feedback Data:")
-        for row in rows:
-            st.write(f"Name: {row[0]}, Roll Number: {row[2]}, College: {row[3]}, Department: {row[4]}, Feedback: {row[8]}, Date: {row[9]}")
-    else:
-        st.write("No data found in the database.")
-
-# Close the connection at the end of the script'''
-conn.close()
+    conn.close()
